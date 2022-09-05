c85bab7d6350e8345f7f348d155994ed09988202
ww <ww@gmail.com>
2022-09-05 20:46:55 +0800

    test code

--- app/person.py
+++ app/person.py
@@ -1,3 +1,8 @@
 
-x = list(("apple", "banana", "cherry"))
-print(x)
+# x = list(("apple", "banana", "cherry"))
+# print(x)
+
+
+age1 = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age1))
